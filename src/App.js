df05dd7d88b7e2314e6b4bb3fa568e213ca16ae4
Duy Huynh <hdduy97@gmail.com>
2021-04-27 14:29:58 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const onClick = async () => setCount(count + 1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Count: {count}</div>
+      <div className="double-count">Double count: {count * 2 }</div>
+      <button onClick={onClick}>Increase count</button>
     </div>
-  );
+  )
 }
 
 export default App;
