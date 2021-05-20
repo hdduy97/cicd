77b7637fd57e9aff9513fdcd62929a4b2191e356
Duy Huynh <hdduy97@gmail.com>
2021-05-21 00:28:16 +0700

    update

--- src/components/page/Category/index.js
+++ src/components/page/Category/index.js
@@ -64,12 +64,24 @@ const Index = () => {
     }
   }
 
+  const imgStyle = {
+    width: '216px',
+    height: '269px'
+  }
+
   const productsRender = products.map(product => (
     <li key={product.id} className="item product product-item">
       <div className="product-item-info">
         <span className="product-image-container" style={{width: '240px'}}>
           <span className="product-image-wrapper">
-            <img className="product-image-photo" src={`${process.env.REACT_APP_PRODUCT_IMAGE}/${product.media_gallery_entries[0].file}`} alt={product.name} />
+            <img 
+              className="product-image-photo"
+              src={`${process.env.REACT_APP_PRODUCT_IMAGE}/${product.media_gallery_entries[0].file}`} 
+              alt={product.name} 
+              width="216"
+              height="269"
+              style={imgStyle}
+            />
           </span>
         </span>
         <div className="product details product-item-details">
